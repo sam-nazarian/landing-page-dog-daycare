e0a9c561aa83967ac0e2826bb23d04f37827d27f
samanfathnazarian <saman.nazarian17@gmail.com>
2022-08-26 05:00:31 -0500

    implemented map & trust sections

--- deleted.md
+++ deleted.md
@@ -1,3 +1,27 @@
+header of html
+
+  <!-- FAVICONS -->
+  <!-- <link rel="icon" href="img/favicon.png" /> -->
+  <!-- <link rel="apple-touch-icon" href="img/apple-touch-icon.png" /> -->
+  <!-- for Android -->
+  <!-- <link rel="manifest" href="manifest.webmanifest"> -->
+
+    <!-- necessorly line for media queries to work, without it doesn't work on mobile devices as it zooms page out until it fits the screen, we want to make the design the device smaller -->
+  <!-- makes page match screens width, wihout zooming out -->
+
+    <!-- summary of content, apear in search engines -->
+  <!-- meta is metaData, data that describes other data -->
+
+
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
+  <!-- <link rel="stylesheet" href="css/queries.css"> -->
+
+
+    <!-- IONICONS JS SCRIPT -->
+  <!-- <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> -->
+
+
 - a safe heaven for your pup
 
 /* .step-img-box::before, .step-img-box::after{
--- js/script.js
+++ js/script.js
@@ -1,154 +1,156 @@
-// const h1 = document.querySelector('.heading-primary');
-// console.log(h1);
+const map = L.map('map', {scrollWheelZoom: false,}).setView([49.883226, -97.155884], 13);
 
-// h1.addEventListener('click', ()=>{
-//   h1.textContent = 'Saman'
-//   h1.style.backgroundColor = 'red';
-//   h1.style.padding = '5rem';
+// https://api.mapbox.com/styles/v1/saman2111/cl79xjeka001714n0hqhmd9mg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FtYW4yMTExIiwiYSI6ImNsMHR2bXo2ZjBmM2ozZG11aDVhejF1MncifQ.3KrUHjHh3FoNFafvfPba_w
+L.tileLayer('https://api.mapbox.com/styles/v1/saman2111/cl79xjeka001714n0hqhmd9mg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FtYW4yMTExIiwiYSI6ImNsMHR2bXo2ZjBmM2ozZG11aDVhejF1MncifQ.3KrUHjHh3FoNFafvfPba_w', {
+  // maxZoom: 12,
+  // attribution: '© OpenStreetMap',
+}).addTo(map);
+
+const marker = L.marker([49.883226, -97.155884]).addTo(map).bindPopup("<h4>Saman's Dog Daycare:</h4><p>120 Young St, Winnipeg, MB R3C 1Y6</p>").openPopup();;
+const marker2 = L.marker([49.865083, -97.198370]).addTo(map).bindPopup("<h4>Saman's Dog Daycare:</h4><p>Lanark Gardens, Winnipeg, MB R3N 1L3</p>").openPopup();;
+
+
+
+// /////////////////////////////////////////////////////////
+// // Set current year
+// const yearEl = document.querySelector('.year');
+// yearEl.textContent = new Date().getFullYear();
+
+// /////////////////////////////////////////////////////////
+// // Make movile navigation work
+// const btnNavEl = document.querySelector('.btn-mobile-nav');
+// const headerEl = document.querySelector('.header');
+
+// btnNavEl.addEventListener('click', function(){
+//   headerEl.classList.toggle('nav-open');
 // })
 
 
+// /////////////////////////////////////////////////////////
+// // Smooth scrolling animations
 
-/////////////////////////////////////////////////////////
-// Set current year
-const yearEl = document.querySelector('.year');
-yearEl.textContent = new Date().getFullYear();
+// // select anchor elm, with href property
+// const allLinks = document.querySelectorAll('a:link');
+// // console.log(allLinks);
 
-/////////////////////////////////////////////////////////
-// Make movile navigation work
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header');
-
-btnNavEl.addEventListener('click', function(){
-  headerEl.classList.toggle('nav-open');
-})
-
-
-/////////////////////////////////////////////////////////
-// Smooth scrolling animations
-
-// select anchor elm, with href property
-const allLinks = document.querySelectorAll('a:link');
-// console.log(allLinks);
-
-allLinks.forEach((link)=>{
-  link.addEventListener('click', function(e){
-    e.preventDefault();
-    const href = link.getAttribute('href');
+// allLinks.forEach((link)=>{
+//   link.addEventListener('click', function(e){
+//     e.preventDefault();
+//     const href = link.getAttribute('href');
     
-    // Scroll back to top
-    if(href === '#') window.scrollTo({top: 0, behavior: 'smooth'});
+//     // Scroll back to top
+//     if(href === '#') window.scrollTo({top: 0, behavior: 'smooth'});
     
-    // Scroll to other links
-    if(href !== '#' && href.startsWith('#') ){
-      const sectionEl = document.querySelector(href);
+//     // Scroll to other links
+//     if(href !== '#' && href.startsWith('#') ){
+//       const sectionEl = document.querySelector(href);
 
-      sectionEl.scrollIntoView({behavior: 'smooth'});
+//       sectionEl.scrollIntoView({behavior: 'smooth'});
 
-      // Close mobile navigation
-      headerEl.classList.remove('nav-open');
-    }
+//       // Close mobile navigation
+//       headerEl.classList.remove('nav-open');
+//     }
 
-    // Another way to Close mobile navigation
-    //only way it contians that class is if menu is open
-    // if(link.classList.contains('main-nav-link')) headerEl.classList.remove('nav-open');
-  })
-})
-
-
-/////////////////////////////////////////////////////////
-// Sticky navigation
-
-const sectionHeroEl = document.querySelector('.section-hero')
-
-const obs = new IntersectionObserver(function(entries){
-  const ent = entries[0];
-  // console.log(ent);
-
-  if(ent.isIntersecting === false) document.body.classList.add('sticky')
-
-  if(ent.isIntersecting === true) document.body.classList.remove('sticky')
-
-}, {
-  // In viewport
-  root: null, 
-  threshold: 0, 
-  //Margin around the root, to make intersection happen sooner
-  rootMargin: '-80px',
-});
-
-obs.observe(sectionHeroEl);
+//     // Another way to Close mobile navigation
+//     //only way it contians that class is if menu is open
+//     // if(link.classList.contains('main-nav-link')) headerEl.classList.remove('nav-open');
+//   })
+// })
 
 
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+// /////////////////////////////////////////////////////////
+// // Sticky navigation
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+// const sectionHeroEl = document.querySelector('.section-hero')
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+// const obs = new IntersectionObserver(function(entries){
+//   const ent = entries[0];
+//   // console.log(ent);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
+//   if(ent.isIntersecting === false) document.body.classList.add('sticky')
 
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+//   if(ent.isIntersecting === true) document.body.classList.remove('sticky')
 
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
+// }, {
+//   // In viewport
+//   root: null, 
+//   threshold: 0, 
+//   //Margin around the root, to make intersection happen sooner
+//   rootMargin: '-80px',
+// });
 
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
+// obs.observe(sectionHeroEl);
 
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
 
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
+// ///////////////////////////////////////////////////////////
+// // Fixing flexbox gap property missing in some Safari versions
+// function checkFlexGap() {
+//   var flex = document.createElement("div");
+//   flex.style.display = "flex";
+//   flex.style.flexDirection = "column";
+//   flex.style.rowGap = "1px";
 
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
+//   flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
 
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
 
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
+//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
+// }
+// checkFlexGap();
 
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
+// // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
+// /*
+// .no-flexbox-gap .main-nav-list li:not(:last-child) {
+//   margin-right: 4.8rem;
+// }
 
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
+// .no-flexbox-gap .list-item:not(:last-child) {
+//   margin-bottom: 1.6rem;
+// }
 
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
+// .no-flexbox-gap .list-icon:not(:last-child) {
+//   margin-right: 1.6rem;
+// }
+
+// .no-flexbox-gap .delivered-faces {
+//   margin-right: 1.6rem;
+// }
+
+// .no-flexbox-gap .meal-attribute:not(:last-child) {
+//   margin-bottom: 2rem;
+// }
+
+// .no-flexbox-gap .meal-icon {
+//   margin-right: 1.6rem;
+// }
+
+// .no-flexbox-gap .footer-row div:not(:last-child) {
+//   margin-right: 6.4rem;
+// }
+
+// .no-flexbox-gap .social-links li:not(:last-child) {
+//   margin-right: 2.4rem;
+// }
+
+// .no-flexbox-gap .footer-nav li:not(:last-child) {
+//   margin-bottom: 2.4rem;
+// }
+
+// @media (max-width: 75em) {
+//   .no-flexbox-gap .main-nav-list li:not(:last-child) {
+//     margin-right: 3.2rem;
+//   }
+// }
+
+// @media (max-width: 59em) {
+//   .no-flexbox-gap .main-nav-list li:not(:last-child) {
+//     margin-right: 0;
+//     margin-bottom: 4.8rem;
+//   }
+// }
+// */
